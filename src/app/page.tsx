ed304fad8d7c5412014c7ea81d1923f67156ce80
roum02 <roum02@ewhain.net>
2025-04-05 23:41:21 +0900

    Modify typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from '@event-mobile-front/components';
 export default function Home() {
   return (
     <div>
-      <Header title={'홈'} />
+      <Header title={'이벤트 목록'} />
       <main>메인페이지</main>
       <footer>푸터</footer>
     </div>
