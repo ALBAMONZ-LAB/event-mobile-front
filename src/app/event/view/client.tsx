328f7744dd934e1b7d6300e0ff22633f04444fa6
이로움 <JK1234~~>
2025-04-07 17:54:51 +0900

    Add Template

--- src/app/event/view/client.tsx
+++ src/app/event/view/client.tsx
@@ -2,24 +2,8 @@
 
 import { getEventDetailPage } from '@event-mobile-front/api';
 import { queryKeys } from '@event-mobile-front/queryKey';
-import {
-  Header,
-  Button,
-  Carousel,
-  FloatingButton,
-  Footer,
-  CustomizedComponent,
-  Image,
-  HeaderProps,
-  ButtonProps,
-  CarouselProps,
-  FloatingButtonProps,
-  FooterProps,
-  ImageProps,
-} from '@event-mobile-front/components';
-import { EventDetailResponse, StyleConfig } from '@event-mobile-front/types';
 import { useQuery } from '@tanstack/react-query';
-import { ComponentType } from 'react';
+import { EventDetailContent } from '@event-mobile-front/template';
 
 export default function EventViewClient({eventId}: {eventId: number}) {
   const queryKey = queryKeys.event.detail(eventId);
@@ -36,94 +20,5 @@ export default function EventViewClient({eventId}: {eventId: number}) {
   );
 }
 
-interface EventDetailContentProps {
- data: EventDetailResponse;
-}
 
-function EventDetailContent({ data }: EventDetailContentProps) {
 
-  const {body, footer } = data.pageJson;
-
-  return (
-    <div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          //background: eventBackground,
-          scrollbarWidth: 'none',
-          msOverflowStyle: 'none',
-        }}
-      >
-        {Array.isArray(body) && body.length > 0 ? (
-          body.map((item, index) => (
-            <section
-              key={`${item.sectionType}_${index}`}
-              style={{
-                ...item.sectionStyle,
-                ...(item.sectionType === 'floatingButton' && { position: 'fixed', width: '360px' }),
-              }}
-            >
-              <RenderComponent {...(item as ComponentData)} />
-            </section>
-          ))
-        ) : (
-          <p>Empty body...</p>
-        )}
-      </div>
-       {footer && <Footer contents={ footer.contents } />}
-    </div>
-  );
-}
-
-const MAPPED_COMPONENTS: {
-  [K in keyof ComponentPropsMap]: ComponentType<ComponentPropsMap[K]>;
-} = {
-  header: Header,
-  image: Image,
-  button: Button,
-  carousel: Carousel,
-  floatingButton: FloatingButton,
-  footer: Footer,
-  custom: CustomizedComponent,
-};
-
-interface ComponentPropsMap {
-  header: HeaderProps;
-  image: ImageProps;
-  button: ButtonProps;
-  carousel: CarouselProps;
-  floatingButton: FloatingButtonProps;
-  footer: FooterProps;
-  custom: object;
-}
-
-type ComponentDataMap = {
-  [K in keyof ComponentPropsMap]: {
-    sectionType: K;
-    orderNo?: number;
-    sectionStyle?: StyleConfig;
-    children?: ComponentData[];
-  } & ComponentPropsMap[K];
-};
-
-type ComponentData = ComponentDataMap[keyof ComponentDataMap];
-
-const RenderComponent = ({ ...props }: ComponentData) => {
-  if (!props.sectionType) return null;
-
-  const Component = MAPPED_COMPONENTS[props.sectionType] as ComponentType<any>;
-  if (!Component) return null;
-
-  return (
-    <Component {...props}>
-      {/* {'children' in props &&
-        props.children?.map((child, i) => (
-          <span key={`${child.sectionType}_${i}`}>
-            <RenderComponent {...child} />
-          </span>
-        ))} */}
-    </Component>
-  );
-};
